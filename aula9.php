670a15734f6b62c556973aaf381b9145f0f3647a
Michael Martins <michaelfm21@gmail.com>
2021-07-17 22:41:33 -0300

    spaceship operator

---
+++ aula9.php
@@ -0,0 +1,10 @@
+<?php 
+
+/**
+ * Spaceship operator <=>
+ * Retorna 0: valores de ambos os lados iguais.
+ * Retorna 1: valor da esquerda é maior.
+ * Retorna -1: valor à direita é maior.
+ */
+
+ echo 9 <=> 10;
